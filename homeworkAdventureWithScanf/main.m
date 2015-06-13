fc45f14d8f67c428ef41ace02c771e5bb4b4164b
Varindra <varindrahart@gmail.com>
2015-06-13 10:29:58 -0400

    Harry Potter Simulator v3

--- homeworkAdventureWithScanf/main.m
+++ homeworkAdventureWithScanf/main.m
@@ -12,35 +12,41 @@ int main(int argc, const char * argv[]) {
     @autoreleasepool {
         // insert code here...
         NSLog(@"Hello, World!");
+        printf("%d",arc4random());
+        printf("%d",arc4random());
+        printf("%d",arc4random());
+        printf("%d",arc4random());
+        printf("%d",arc4random());
         char user[100];
+        //int k=-1;
         printf("Ok lets begin. Please enter your name to play this simulator: ");
         scanf("%99[^\n]%*c",user);
         
         printf("%s, ever wanted to be Harry Potter? \n\nWell want no more!\n",user);
-        int i;
+        int i=1;
         int j;
         //int k;
+        while (i!=0){
         printf("\nHarry! You need to decide, would you like a dragon or an owl? Enter 1 for dragon and 0 for owl: ");
         scanf("%d",&i);
-        if (i!=0){
-            printf("The dragon works for Voldermort (obviously!). Long story short, you're more of a Nevil Longbottom, let's be honest");
-            return 0;
+        printf("The dragon works for Voldermort (obviously!). Long story short, you're more of a Nevil Longbottom, let's be honest");
         }
-        else{
-            printf("\nGood choice Harry. But this is only the beginning..by the way, your lightning bolt is showing\n");
-        }
+        printf("\nGood choice Harry. But this is only the beginning..by the way, your lightning bolt is showing\n");
         
+        while(i!=1){
         printf("\nThe door to Gryphndor common room has disappeared! It's either on the left or the right. Which do you choose? O for left, 1 for right: \n");
         scanf("%d",&i);
-        if (i!=1){
+        //if (i!=1){
             printf("\nYou've chosen the girls bathroom!..The bathroom works for Voldermort. Tough luck buddy");
-            return 0;
+            //return 0;
         }
+        
+        while(i!=0){
         printf("\nGood choice but the door has led you to Slytherin's common room and Draco challenges you! He's been going off on 'mudblood this, mudblood's that'\nWhat a tool right?\nAnyways, you've got to defend yourself, choose a spell! 1 for Avada Kadavra 0 for Wingardium leviosa: ");
         scanf("%d", &i);
-        if( i!=0){
+        //if( i!=0){
             printf("\n\nDude..that's brutal, and brutal works for Voldermot..adios");
-            return 0;
+            //return 0;
         }
         BOOL fight;
         printf("\nIts leviOosaah Harry! (check this link out for reference, it's hilarious: www.youtube.com/watch?v=FWtO0cfgewY)\n");
@@ -58,7 +64,7 @@ int main(int argc, const char * argv[]) {
             int bam;
             
             while (VoldHealth>0 && yourHealth>0){
-                printf("\nPick an attack! Enter either 1 2 or 3: ");
+                printf("\nPick an attack! Enter either 1 2 or 3: \n1) Stupefy\n2) Patronus\n3) Expelliarmus\n");
                 scanf("%d%*c",&bam);
                 switch (bam) {
                     case 1:
